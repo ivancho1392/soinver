790cfa93e7f786d2199a8979d63521136d07c867
ivancho1392 <ivancho1392@gmail.com>
2023-08-13 22:37:59 -0500

    icons for index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,7 @@
 import styles from "../styles/Home.module.css";
+import { FaHandshake, FaTools, FaGears } from 'react-icons/fa';
+import { MdEngineering } from 'react-icons/md';
+import { BsGearFill } from 'react-icons/bs';
 
 export default function Home() {
   return (
@@ -8,7 +11,7 @@ export default function Home() {
         <div className={styles.homeMenus}>
           <div className={styles.homeM1}>
             <div className={styles.homeM2}>
-              <p>logo</p>
+              <p className={styles.homeLogos}><FaTools size={35}/></p>
               <p className={styles.homeM3}>+400</p>
             </div>
             <div className={styles.homeM4}>Equipos instalados y/o ajustados</div>
@@ -16,18 +19,18 @@ export default function Home() {
           </div>
           <div className={styles.homeM1}>
             <div className={styles.homeM2}>
-              <p>logo</p>
+            <p className={styles.homeLogos}><BsGearFill size={35}/></p>
               <p className={styles.homeM3}>+100</p>
             </div>
             <div className={styles.homeM4}>Equipos instalados y/o ajustados</div>
             <div className={styles.homeM3}>Panamá</div>
           </div>
           <div className={styles.homeM1}>
-            <p>logo</p>
+            <p className={styles.homeLogos}><MdEngineering size={50}/></p>
             <p className={styles.homeM4}>Personal altamente calificado y con amplia experiencia</p>
           </div>
           <div className={styles.homeM1}>
-            <p>logo</p>
+            <p className={styles.homeLogos}><FaHandshake size={50}/></p>
             <p className={styles.homeM4}>El mejor servicio en equipos de transporte vertical</p>
           </div>
         </div>
