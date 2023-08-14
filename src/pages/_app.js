eddaf0ad47a2e8d2a405538da58561ced08d583d
ivancho1392 <ivancho1392@gmail.com>
2023-08-13 22:09:45 -0500

    Initial commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
-import '@styles/ @logos/ @components/ @common/ @layout/styles/globals.css'
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { Manjari } from 'next/font/google'
+ 
+const manjari = Manjari({
+  weight: ['100','400'],
+  subsets: ['latin'],
+})
+ 
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <main className={manjari.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
