9c1205b9593f36d2ff953bd2b5dd8bc7cfad4f8f
ivancho1392 <ivancho1392@gmail.com>
2023-08-15 12:12:54 -0500

    subtittle

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,67 @@
 import styles from "../styles/Home.module.css";
-import { FaHandshake, FaTools, FaGears } from 'react-icons/fa';
-import { MdEngineering } from 'react-icons/md';
-import { BsGearFill } from 'react-icons/bs';
+import { FaHandshake, FaTools, FaGears } from "react-icons/fa";
+import { MdEngineering } from "react-icons/md";
+import { BsGearFill } from "react-icons/bs";
 import Layout from "../Components/Layout";
 
 export default function Home() {
   return (
-    <Layout> 
-    <div className={styles.homepage}>
-      <div className={styles.homeBackground}>
-        <h1 className={styles.homeTittle}>Elevadores y Escaleras Eléctricas</h1>
-        <div className={styles.homeMenus}>
-          <div className={styles.homeM1}>
-            <div className={styles.homeM2}>
-              <p className={styles.homeLogos}><FaTools size={35}/></p>
-              <p className={styles.homeM3}>+400</p>
+    <Layout>
+      <div className={styles.homepage}>
+        {/* Primera Seccion */}
+        <div className={styles.homeBackground}>
+          <h1 className={styles.homeTittle}>
+            Elevadores y Escaleras Eléctricas
+          </h1>
+          <h1 className={styles.homeTittle2}>
+            Soluciones de Ingeniería Vertical, el mejor servicio en equipos de transporte vertical
+          </h1>
+          <div className={styles.homeMenus}>
+            <div className={styles.homeM1}>
+              <div className={styles.homeM2}>
+                <p className={styles.homeLogos}>
+                  <FaTools size={35} />
+                </p>
+                <p className={styles.homeM3}>+400</p>
+              </div>
+              <div className={styles.homeM4}>
+                Equipos instalados y/o ajustados
+              </div>
+              <div className={styles.homeM3}>Colombia</div>
             </div>
-            <div className={styles.homeM4}>Equipos instalados y/o ajustados</div>
-            <div className={styles.homeM3}>Colombia</div>
-          </div>
-          <div className={styles.homeM1}>
-            <div className={styles.homeM2}>
-            <p className={styles.homeLogos}><BsGearFill size={35}/></p>
-              <p className={styles.homeM3}>+100</p>
+            <div className={styles.homeM1}>
+              <div className={styles.homeM2}>
+                <p className={styles.homeLogos}>
+                  <BsGearFill size={35} />
+                </p>
+                <p className={styles.homeM3}>+100</p>
+              </div>
+              <div className={styles.homeM4}>
+                Equipos instalados y/o ajustados
+              </div>
+              <div className={styles.homeM3}>Panamá</div>
             </div>
-            <div className={styles.homeM4}>Equipos instalados y/o ajustados</div>
-            <div className={styles.homeM3}>Panamá</div>
-          </div>
-          <div className={styles.homeM1}>
-            <p className={styles.homeLogos}><MdEngineering size={50}/></p>
-            <p className={styles.homeM4}>Personal altamente calificado y con amplia experiencia</p>
-          </div>
-          <div className={styles.homeM1}>
-            <p className={styles.homeLogos}><FaHandshake size={50}/></p>
-            <p className={styles.homeM4}>El mejor servicio en equipos de transporte vertical</p>
+            <div className={styles.homeM1}>
+              <p className={styles.homeLogos}>
+                <MdEngineering size={50} />
+              </p>
+              <p className={styles.homeM4}>
+                Personal altamente calificado y con amplia experiencia
+              </p>
+            </div>
+            <div className={styles.homeM1}>
+              <p className={styles.homeLogos}>
+                <FaHandshake size={50} />
+              </p>
+              <p className={styles.homeM4}>
+                El mejor servicio en equipos de transporte vertical
+              </p>
+            </div>
           </div>
         </div>
+        {/* Segunda Seccion */}
+        <div>Seccion siguiente</div>
       </div>
-      <div>Seccion siguiente</div>
-      </div>
-      </Layout>
-  )
+    </Layout>
+  );
 }
