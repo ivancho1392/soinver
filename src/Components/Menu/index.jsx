c35a3e47744be758f6c20f745cbf76bedf484452
ivancho1392 <ivancho1392@gmail.com>
2023-09-10 06:21:46 -0500

    menu logos

--- src/Components/Menu/index.jsx
+++ src/Components/Menu/index.jsx
@@ -2,52 +2,50 @@ import { useContext } from "react";
 import styles from "./styles.module.css";
 import { SoinverContext } from "../../Context";
 import Link from "next/link";
+import { GrWaypoint } from "react-icons/gr";
+import { HiOutlinePhoto, HiOutlineWrench } from "react-icons/hi2";
+import { AiOutlineTeam } from "react-icons/ai";
+import { BsWhatsapp } from "react-icons/bs";
 
 const Menu = () => {
   const context = useContext(SoinverContext);
 
   return (
     <div className={context.isMenuOpen ? styles.menuopen : styles.menuclose}>
-      <div className={styles.menuitem}>
-        <div className={styles.icon}>Icono 1</div>
-        <div className={styles.name}>
-          <Link href="/#servicios" onClick={context.closeMenu}>
-            Nuestros Servicios
-          </Link>
+      <Link
+        href="/#servicios"
+        onClick={context.closeMenu}
+        className={styles.menuitem}
+      >
+        <div className={styles.icon}><HiOutlineWrench/></div>
+        <div className={styles.name}>Nuestros Servicios</div>
+      </Link>
+      <Link
+        href="/#trayectoria"
+        onClick={context.closeMenu}
+        className={styles.menuitem}
+      >
+        <div className={styles.icon}>
+          <GrWaypoint />
         </div>
-      </div>
-      <div className={styles.menuitem}>
-        <div className={styles.icon}>Icono 2</div>
-        <div className={styles.name}>
-          <Link href="/#trayectoria" onClick={context.closeMenu}>
-            Trayectoria de nuestra compañia
-          </Link>
-        </div>
-      </div>
-      <div className={styles.menuitem}>
-        <div className={styles.icon}>Icono 3</div>
-        <div className={styles.name}>
-          <Link href="/#equipo" onClick={context.closeMenu}>
-            Equipo Soinver{" "}
-          </Link>
-        </div>
-      </div>
-      <div className={styles.menuitem}>
-        <div className={styles.icon}>Icono 4</div>
-        <div className={styles.name}>
-          <Link href="/" onClick={context.closeMenu}>
-            Galeria de Proyectos{" "}
-          </Link>
-        </div>
-      </div>
-      <div className={styles.menuitem}>
-        <div className={styles.icon}>Icono 5</div>
-        <div className={styles.name}>
-          <Link href="/" onClick={context.closeMenu}>
-            Contacto{" "}
-          </Link>
-        </div>
-      </div>
+        <div className={styles.name}>Trayectoria de nuestra compañia</div>
+      </Link>
+      <Link
+        href="/#equipo"
+        onClick={context.closeMenu}
+        className={styles.menuitem}
+      >
+        <div className={styles.icon}><AiOutlineTeam/></div>
+        <div className={styles.name}>Equipo Soinver</div>
+      </Link>
+      <Link href="/" onClick={context.closeMenu} className={styles.menuitem}>
+        <div className={styles.icon}><HiOutlinePhoto/></div>
+        <div className={styles.name}>Galeria de Proyectos</div>
+      </Link>
+      <Link href="/" onClick={context.closeMenu} className={styles.menuitem}>
+        <div className={styles.icon}><BsWhatsapp/></div>
+        <div className={styles.name}>Contacto</div>
+      </Link>
     </div>
   );
 };
